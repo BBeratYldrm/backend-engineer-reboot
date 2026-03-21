134f56645282350a5172b8e015f075ff807a153a
BBeratYLdrm <bberatyldrm@gmail.com>
2026-03-21 20:13:24 +0900

    algo: contains duplicate with explanation

---
+++ src/main/java/com/berat/reboot/algorithms/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package com.berat.reboot.algorithms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) { //  I will take an array
+
+        Set<Integer> set = new HashSet<>(); // I will keep in here what i see
+
+        for  (int num : nums) { // I will start to see
+            if (set.contains(num)) { // When i'm looking, i will check did i see before ?
+                return true; // If i saw before, it will be on the set, so there is duplicate
+            }
+            set.add(num); // When i'm looking, if i didn't see before i will add that to list
+        }
+
+        return false; // I will return false if there is no duplicate
+    }
+
+}
